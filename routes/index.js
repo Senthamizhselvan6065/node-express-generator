1bc962319c96cc031047593803c094f09a46d9c5
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2023-06-02 18:03:32 +0530

    index

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send({
-    message: "Express"
-  })
+  res.status(200).send(`<h1>Welcome To Express<h1/>`)
 });
 
 module.exports = router;
